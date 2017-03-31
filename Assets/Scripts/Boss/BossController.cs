eccc6d1e129a13184082e9b36f8f6fb8e3ceea3f
Tony <toay789@gmail.com>
2017-03-31 14:40:39 -0500

    boss script change

---
+++ Assets/Scripts/Boss/BossController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour {
+
+    private Renderer r;
+    private GameObject Player;
+
+    // Use this for initialization
+    void Start() {
+        r = GetComponent<Renderer>();
+        r.enabled = true;
+        Player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (r.enabled) {
+            if (BossValues.Health <= 0)
+                r.enabled = false;
+            else
+                transform.Translate(Player.transform.position.normalized * BossValues.Speed * Time.deltaTime);
+        }
+    }
+}
